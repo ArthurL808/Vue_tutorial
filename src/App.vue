55e7e19cbcbac497fe6ea60d3c7e7e777a55d257
Arthur <kingart808@gmail.com>
2021-11-03 15:41:12 -1000

    started vue tutorial to make a task tracker

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,77 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header
+    @toggle-add-task="toggleAddTask"
+    title="Task Tracker"
+    :showAddTask="showAddTask"
+  />
+  <div v-show="showAddTask">
+    <AddTask @add-task="addTask" />
+  </div>
+  <Tasks
+    @toggle-reminder="toggleReminder"
+    @delete-task="deleteTask"
+    :tasks="tasks"
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Header,
+    Tasks,
+    AddTask,
+  },
+  data() {
+    return {
+      tasks: [],
+      showAddTask: false,
+    };
+  },
+  methods: {
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask;
+    },
+    addTask(task) {
+      this.tasks = [...this.tasks, task];
+    },
+    deleteTask(id) {
+      if (confirm("are you sure?")) {
+        this.tasks = this.tasks.filter((task) => task.id !== id);
+      }
+    },
+    toggleReminder(id) {
+      this.tasks = this.tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      );
+    },
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "Walk the dog",
+        day: "Nov 3 at 2:00pm",
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: "Take out the trash.",
+        day: "Nov 5 at 10:30am",
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: "Call your sister.",
+        day: "Nov 5 at 12:00pm",
+        reminder: false,
+      },
+    ];
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Task.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    @dblclick="$emit('toggle-reminder', task.id)"
+    :class="[task.reminder ? 'reminder' : '', 'task']"
+  >
+    <h3>
+      {{ task.text }}
+      <i @click="$emit('delete-task', task.id)" class="close">x</i>
+    </h3>
+    <p>{{ task.day }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "Task",
+  props: {
+    task: Object,
+  }
+};
+</script>
+
+<style scoped>
+.task {
+  background: lightgray;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+
+.task.reminder {
+  border-left: 5px solid green;
+}
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.close {
+  color: red;
+}
+</style>
